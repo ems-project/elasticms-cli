a4480f6f0b4614790263f7ed398fb67d1c3974d1
mdk <mathieu.dekeyzer@smals.be>
2022-02-18 09:53:23 +0100

    feat: common bundle

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    EMS\CommonBundle\EMSCommonBundle::class => ['all' => true],
 ];
