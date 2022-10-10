31387d425719a9db3fc095505cbb9bd2d9154db1
Isabelle Michiels <isabelle.michiels@smals.be>
2022-10-10 11:11:38 +0200

    fix: import of 401 and 404 + add info on url error

--- src/Client/WebToElasticms/Config/ConfigManager.php
+++ src/Client/WebToElasticms/Config/ConfigManager.php
@@ -270,7 +270,7 @@ class ConfigManager
         $stream->seek(0);
         $hash = $this->coreApi->file()->uploadStream($stream, $filename, $mimeType);
 
-        if (\is_null($hash) or 0 === \strlen($hash)) {
+        if (0 === \strlen($hash)) {
             throw new \RuntimeException('Unexpected empty hash');
         }
 
