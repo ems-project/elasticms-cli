bea2cacdbb8ed4040b12fe99a91348d80ceb261b
Mathieu De Keyzer <me@theus.be>
2022-11-23 07:47:55 +0100

    feat: apple photos migrate (#18)
    * feat: command's service
    
    * feat: loop on all photos
    
    * ref: interface and abstract
    
    * feat: basic document with filename
    
    * feat: save photo
    
    * fix: option
    
    * feat: preview file
    
    * feat: --upload-original flag
    
    * feat: member of albums
    
    * feat: $modificationDate, $addedDate, $location
    
    * fix: date format

--- src/Commands.php
+++ src/Commands.php
@@ -7,6 +7,7 @@ namespace App;
 class Commands
 {
     public const WEB_MIGRATION = 'emscli:web:migrate';
+    public const APPLE_PHOTOS_MIGRATION = 'emscli:apple-photos:migrate';
     public const WEB_AUDIT = 'emscli:web:audit';
     public const DOCUMENTS_UPDATE = 'emscli:documents:update';
 }
