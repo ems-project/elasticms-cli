00f3f81ec1b05797c6410d44d0222266edee0a81
David mattei <d.mattei89@gmail.com>
2024-11-04 11:56:27 +0100

    fix(cli/file-import): improvements and config(s) hashes or files (#1056)

---
+++ src/Client/File/FileReaderImportConfig.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\CLI\Client\File;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FileReaderImportConfig
+{
+    /**
+     * @param array<string, mixed> $defaultData
+     * @param int[]                $excludeRows
+     */
+    private function __construct(
+        public array $defaultData = [],
+        public bool $deleteMissingDocuments = false,
+        public ?string $delimiter = null,
+        public ?string $encoding = null,
+        public array $excludeRows = [],
+        public bool $generateHash = false,
+        public ?string $ouuidExpression = "row['ouuid']",
+        public ?string $ouuidPrefix = null,
+    ) {
+    }
+
+    /**
+     * @param array<string, mixed> $config
+     */
+    public static function createFromArray(array $config): self
+    {
+        $optionsResolver = new OptionsResolver();
+        $optionsResolver
+            ->setDefaults([
+                'default_data' => [],
+                'delete_missing_documents' => false,
+                'delimiter' => null,
+                'encoding' => null,
+                'exclude_rows' => [],
+                'generate_hash' => false,
+                'ouuid_expression' => 'row[\'ouuid\']',
+                'ouuid_prefix' => null,
+            ])
+            ->setAllowedTypes('delete_missing_documents', 'bool')
+            ->setAllowedTypes('generate_hash', 'bool')
+            ->setAllowedTypes('ouuid_expression', ['string', 'null'])
+            ->setAllowedTypes('ouuid_prefix', ['string', 'null'])
+        ;
+
+        $options = $optionsResolver->resolve($config);
+
+        return new self(
+            defaultData: $options['default_data'],
+            deleteMissingDocuments: $options['delete_missing_documents'],
+            delimiter: $options['delimiter'],
+            encoding: $options['encoding'],
+            excludeRows: $options['exclude_rows'],
+            generateHash: $options['generate_hash'],
+            ouuidExpression: $options['ouuid_expression'],
+            ouuidPrefix: $options['ouuid_prefix'],
+        );
+    }
+}
